9a9290e414489481f5a9130607b35810c638effb
chessai <chessai1996@gmail.com>
2018-04-14 12:38:06 -0400

    remove unused stuff

--- Pokemon.hs
+++ Pokemon.hs
@@ -1,21 +1,16 @@
 {-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE EmptyCase     #-}
 {-# LANGUAGE GADTs         #-}
-{-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE PolyKinds     #-}
 {-# LANGUAGE TypeFamilies  #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeInType    #-}
 
 module Pokemon where
 
 import Data.Kind (Constraint)
-import Type.Reflection ((:~:)(..))
 
 data PType = None | Fire | Water | Grass
 
 -- If 'a' and 'b' are different - the constraint
--- cannot be solved as the construction '() ~ Int'
+-- cannot be solved, as the construction '() ~ Int'
 -- is pathological.
 type family Different a b :: Constraint where
   Different a a = () ~ Int
